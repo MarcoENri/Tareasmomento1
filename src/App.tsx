5ebff01d8488fca371e4e361bdbfc99977f8d303
MarcoENri <130495181+MarcoENri@users.noreply.github.com>
2024-04-08 10:16:53 -0500

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-<<<<<<< HEAD
+
+
+import React from 'react';
 
 export const COLORS = [
   'black',
@@ -16,26 +18,16 @@ export const COLORS = [
 type Colors = typeof COLORS[number];
 
 export const colorCode = (color: Colors) => COLORS.indexOf(color);
-=======
-import TwoFer from './TwoFer'; 
->>>>>>> a9299191c66601bebc48ea3a74b98dd8b55d05b2
 
 function App() {
   return (
     <div>
-<<<<<<< HEAD
       <h1>Color Code</h1>
       <ul>
         {COLORS.map((color, index) => (
           <li key={index}>{color}: {colorCode(color)}</li>
         ))}
       </ul>
-=======
-      <h1>Welcome to My App</h1>
-      <TwoFer /> {"One for you, one for me." }
-      <TwoFer name="Alice" /> {  "One for Alice, one for me"}
-      <TwoFer name="Bob" /> { "One for Bob, one for me." }
->>>>>>> a9299191c66601bebc48ea3a74b98dd8b55d05b2
     </div>
   );
 }
