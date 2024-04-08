a9299191c66601bebc48ea3a74b98dd8b55d05b2
MarcoENri <marcoenriquez561290@gmail.com>
2024-04-08 09:52:49 -0500

    tarea2

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,5 @@
 import TwoFer from './TwoFer'; 
 
-export const COLORS = [
-  'black',
-  'brown',
-  'red',
-  'orange',
-  'yellow',
-  'green',
-  'blue',
-  'violet',
-  'grey',
-  'white',
-] as const;
-
-type Colors = typeof COLORS[number];
-
-export const colorCode = (color: Colors) => COLORS.indexOf(color);
-
 function App() {
   return (
     <div>
@@ -24,13 +7,6 @@ function App() {
       <TwoFer /> {"One for you, one for me." }
       <TwoFer name="Alice" /> {  "One for Alice, one for me"}
       <TwoFer name="Bob" /> { "One for Bob, one for me." }
-=======
-      <h1>Color Code</h1>
-      <ul>
-        {COLORS.map((color, index) => (
-          <li key={index}>{color}: {colorCode(color)}</li>
-        ))}
-      </ul>
     </div>
   );
 }
